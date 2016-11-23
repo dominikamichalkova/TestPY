961f36bfa40d1a17421037d6ef21af49d6dafed8
Dominika <dmichalk@sk.ibm.com>
2016-11-23 12:12:59 +0100

    modified test1 chart plot

--- test1.py
+++ test1.py
@@ -4,9 +4,11 @@ import matplotlib.pyplot as plt
 X = np.linspace(-np.pi, np.pi, 256, endpoint=True)
 C,S = np.cos(X), np.sin(X)
 
-plt.plot(X,C)
-plt.plot(X,S)
+plt.plot(X,C,color='green',linestyle='dashed')
+plt.plot(X,S,color='blue',marker='o',markerfacecolor='red')
 
 plt.show()
+
+help(plt.plot)
 
 
