23375fa84ffcc3209d8914a462526eed830e8cf7
Dominika <dmichalk@sk.ibm.com>
2016-11-24 14:53:14 +0100

    modified skuska

--- skuska.py
+++ skuska.py
@@ -1,2 +1,25 @@
-x = 1
-if x == 1: print("x is 1.")
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+limit_rows   = 800
+df           = pd.read_csv("C:\Users\IBM_ADMIN\PycharmProjects\Python1\data",dtype={"PassengerID":int,
+                                                    "Survived":bool,
+                                                    "Pclass":int,
+                                                    "Name":str,
+                                                    "Sex":str,
+                                                    "Age":int,
+                                                    "SibSp":int,
+                                                    "Parch":int,
+                                                    "Ticket":str,
+                                                    "Fare":float,
+                                                    "Cabin":str,
+                                                    "Embarked":str}, nrows=limit_rows)
+
+unique_ids   = pd.Series(df['ncodpers'].unique())
+limit_people = 1.2e4
+unique_id    = unique_ids.sample(n=limit_people)
+df           = df[df.ncodpers.isin(unique_id)]
+df.describe()
