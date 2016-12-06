b5a3c052ecadd2d3e08ccaa8d789f8dbc06e09b5
Dominika <dmichalk@sk.ibm.com>
2016-12-06 15:40:45 +0100

    add new pandas scripts

--- help.py
+++ help.py
@@ -1,3 +1,13 @@
 import seaborn as sns
+import pandas as pd
+import numpy as np
 
-help(sns.heatmap)
+#help(sns.heatmap)
+
+import matplotlib.pyplot as plt
+
+help(plt.subplots)
+
+df2 = pd.DataFrame(np.random.randn(10, 5))
+
+print df2
--- hrdata.py
+++ hrdata.py
@@ -7,10 +7,34 @@ import seaborn as sns
 
 df = pd.read_csv('C:/Users/IBM_ADMIN/PycharmProjects/Python1/data/hrdata.csv')
 
-df.info()
+df.info() # summary of df
+
+dn=df.head(2) #returns a dataframe holding first n rows of df, pandas pckg
+print dn;
+
+# print df.T; #transpose df
+
+#panda attributes without applied on entire data set
+# print df.T; #transpose df
+print 'number of dimensions:',df.ndim;
+print 'tuple representing dimensionality', df.shape;  #no of (rows,columns)
+print df.ftypes; #indicator of ftypes - sparse/dense and dtype, in the object
+print df.dtypes; #data types, int, float, char
+print 'no of elements in the NDframe',df.size; #no of rows
+print df.axes; #returns a list with row/column axis labels
+print df['sales'].size;
+print df['satisfaction_level'].dtype;
+
+
+#methods
+print 'unique sales fields:',df['sales'].unique();
+print df.mean();
+print df['average_montly_hours'].mean()/30 #daily working hours avg
 
 print('# of people left = {}'.format(df[df['left']==1].size))
 print('# of people stayed = {}'.format(df[df['left']==0].size))
+print('# of people work for HR = {}'.format(df[df['sales']=='hr'].size))
+print('protion of people who left in 5 years = {}%'.format(int(df[df['left']==1].size/df.size*100)))
 
 corrmat = df.corr()
 f, ax = plt.subplots(figsize=(5, 5))
